aae111d742dd4df8cb37dc541e3b015db130cae1
Eldosyano <eldossadyrbaev843@gmail.com>
2023-01-30 13:29:52 +0600

    finished project

---
+++ telegram.php
@@ -0,0 +1,29 @@
+gi
+<?php
+
+/* https://api.telegram.org/bot5769515364:AAHrbcgQ_VEyj_UswgGImn4OEax19t9HtQk/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5769515364:AAHrbcgQ_VEyj_UswgGImn4OEax19t9HtQk";
+$chat_id = "-801936014";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
